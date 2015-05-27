0c5a7b7a7f03122db41493500d1bc2bf434e7142
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-05-27 18:23:58 +0200

    nowrap

--- management/commands/ksp_fosdem14.py
+++ management/commands/ksp_fosdem14.py
@@ -18,7 +18,7 @@ class Command(BaseCommand):
     help = 'Importe les clefs du FOSDEM 2014'
 
     def handle(self, *args, **options):
-        ksp, _ = KeySigningParty.objects.get_or_create(name='FOSDEM 2015', slug='fosdem15')
+        ksp, _ = KeySigningParty.objects.get_or_create(name='FOSDEM 2014', slug='fosdem14')
         r = requests.get('https://ksp.fosdem.org/2014/files/ksp-fosdem2014.txt')
         r.raise_for_status()
         ksp.add_keys([l.split('/')[1].split()[0] for l in r.content.decode('utf-8').split('\n') if l.startswith('pub')])
