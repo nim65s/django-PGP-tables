9b5da763eec38ffc0d696a496432b50931f99ccd
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-06-12 10:32:41 +0200

    détail

--- management/commands/check_signatures.py
+++ management/commands/check_signatures.py
@@ -10,10 +10,10 @@ class Command(BaseCommand):
     help = 'Vérifie les signatures manquantes'
 
     def add_arguments(self, parser):
-        parser.add_argument('ksp', choices=['*'] + [u'net7', u'fosdem15', u'CdL14', u'fosdem14'], default='*')
+        parser.add_argument('ksp', nargs='?', choices=[u'net7', u'fosdem15', u'CdL14', u'fosdem14'])
 
     def handle(self, *args, **options):
         # call(['gpg2', '--refresh-keys'] + [k.id for k in Key.objects.all()])
-        keys = Key.objects if options['ksp'] == '*' else KeySigningParty.objects.get(slug=options['ksp']).keys
+        keys = Key.objects if options['ksp'] is None else KeySigningParty.objects.get(slug=options['ksp']).keys
         for key in keys.all():
             key.check_signatures()
