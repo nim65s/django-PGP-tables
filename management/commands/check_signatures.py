03b1e73d513114560df57986dc31e8b1ca63b281
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-06-12 10:13:49 +0200

    check_signatures pour une seule ksp

--- management/commands/check_signatures.py
+++ management/commands/check_signatures.py
@@ -3,13 +3,17 @@
 from __future__ import unicode_literals
 
 from django.core.management.base import BaseCommand
-from gpg.models import Key
+from gpg.models import Key, KeySigningParty
 
 
 class Command(BaseCommand):
     help = 'Vérifie les signatures manquantes'
 
+    def add_arguments(self, parser):
+        parser.add_argument('ksp', choices=['*'] + [u'net7', u'fosdem15', u'CdL14', u'fosdem14'], default='*')
+
     def handle(self, *args, **options):
         # call(['gpg2', '--refresh-keys'] + [k.id for k in Key.objects.all()])
-        for key in Key.objects.all():
+        keys = Key.objects if options['ksp'] == '*' else KeySigningParty.objects.get(slug=options['ksp']).keys
+        for key in keys.all():
             key.check_signatures()
