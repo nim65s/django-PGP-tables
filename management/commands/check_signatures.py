9d4b7159fa98da76f2ed27306d62db10994be569
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-06-24 12:48:09 +0200

    refresh keys, mais juste celles qui comptent

--- management/commands/check_signatures.py
+++ management/commands/check_signatures.py
@@ -2,6 +2,8 @@
 
 from __future__ import unicode_literals
 
+from subprocess import call
+
 from django.core.management.base import BaseCommand
 from gpg.models import Key, KeySigningParty
 
@@ -13,7 +15,7 @@ class Command(BaseCommand):
         parser.add_argument('ksp', nargs='?', choices=[u'net7', u'fosdem15', u'CdL14', u'fosdem14'])
 
     def handle(self, *args, **options):
-        call(['gpg2', '--refresh-keys'] + [k.id for k in Key.objects.all()])
         keys = Key.objects if options['ksp'] is None else KeySigningParty.objects.get(slug=options['ksp']).keys
+        call(['gpg2', '--refresh-keys'] + list(keys.values_list('id', flat=True)))
         for key in keys.all():
             key.check_signatures()
