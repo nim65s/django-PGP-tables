b1da3c2b068ff64a009ec1500ca3c9fc56043bdf
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-12-21 12:44:26 +0100

    urls 1.8

--- urls.py
+++ urls.py
@@ -1,11 +1,12 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.views.generic import DetailView, ListView
 from gpg.models import KeySigningParty
 from gpg.views import KSPKeyDetailView
 
-urlpatterns = patterns('',
+app_name = 'gpg'
+urlpatterns = [
         url(r'^$', ListView.as_view(model=KeySigningParty), name='ksps'),
         url(r'^(?P<slug>[^/]+)$', DetailView.as_view(model=KeySigningParty), name='ksp'),
         url(r'^(?P<slug>[^/]+)/graph$', DetailView.as_view(model=KeySigningParty, template_name_suffix='_graph'), name='graph'),
         url(r'^(?P<slug>[^/]+)/(?P<key_id>[^/]+)$', KSPKeyDetailView.as_view(), name='ksp_key'),
-        )
+        ]
