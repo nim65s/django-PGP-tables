185bc00839e3e7dcb9c337e27399f362c1ae9ad5
Guilhem Saurel <guilhem.saurel@gmail.com>
2016-11-18 00:08:39 +0100

    cache fix #4

--- pgp_tables/management/commands/check_signatures.py
+++ pgp_tables/management/commands/check_signatures.py
@@ -1,15 +1,21 @@
 from subprocess import call
 
+from django.core.cache import cache
+from django.core.cache.utils import make_template_fragment_key
 from django.core.management.base import BaseCommand
 
 from pgp_tables.models import Key, KeySigningParty
+
+
+def all_ksps():
+    return KeySigningParty.objects.values_list('slug', flat=True)
 
 
 class Command(BaseCommand):
     help = 'Vérifie les signatures manquantes'
 
     def add_arguments(self, parser):
-        parser.add_argument('ksp', nargs='?', choices=KeySigningParty.objects.values_list('slug', flat=True))
+        parser.add_argument('ksp', nargs='?', choices=all_ksps())
         parser.add_argument('-r', '--refresh', action='store_true', help='Refresh Keys')
 
     def handle(self, *args, **options):
@@ -18,3 +24,8 @@ class Command(BaseCommand):
             call(['gpg2', '--refresh-keys'] + list(keys.values_list('id', flat=True)))
         for key in keys.all():
             key.check_signatures()
+
+        # Invalidate corresponding caches
+        for ksp in [k.slug for k in all_ksps()] if options['ksp'] is None else [options['slug']]
+            for template in ['ksp_detail', 'ksp_graph']:
+                cache.delete(make_template_fragment_key(template, [ksp])
