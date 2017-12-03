a085e58c5106f10243fb2528a1e253bb20d176e6
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-12-03 23:49:48 +0100

    use requirements.in for setup.py and requirements.txt for tests

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ from setuptools import setup
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
+with open(os.path.join(os.path.dirname(__file__), 'requirements.in')) as requirements:
+    REQUIREMENTS = [req.split('#egg=')[1] if '#egg=' in req else req for req in requirements.readlines()]
+
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
@@ -12,9 +15,7 @@ setup(
     name='django-PGP-tables',
     version='1.0.0',
     packages=['pgp_tables'],
-    install_requires=[
-        'django',
-    ],
+    install_requires=REQUIREMENTS,
     include_package_data=True,
     license='GPL License',
     description='A Django app that shows how PGP keys are cross-signed.',
