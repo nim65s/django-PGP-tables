50e1a39c0f8df023b8e2d12c13d92f3b714888f8
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-01-28 21:05:24 +0100

    urls: use path

--- pgp_tables/urls.py
+++ pgp_tables/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path
 from django.views.generic import DetailView, ListView
 
 from .models import KeySigningParty
@@ -6,9 +6,8 @@ from .views import KSPKeyDetailView
 
 app_name = 'pgp_tables'
 urlpatterns = [
-    url(r'^$', ListView.as_view(model=KeySigningParty), name='ksps'),
-    url(r'^(?P<slug>[^/]+)$', DetailView.as_view(model=KeySigningParty), name='ksp'),
-    url(r'^(?P<slug>[^/]+)/graph$', DetailView.as_view(model=KeySigningParty, template_name_suffix='_graph'),
-        name='graph'),
-    url(r'^(?P<slug>[^/]+)/(?P<key_id>[^/]+)$', KSPKeyDetailView.as_view(), name='ksp_key'),
+    path('', ListView.as_view(model=KeySigningParty), name='ksps'),
+    path('<slug:slug>', DetailView.as_view(model=KeySigningParty), name='ksp'),
+    path('<slug:slug>/graph', DetailView.as_view(model=KeySigningParty, template_name_suffix='_graph'), name='graph'),
+    path('<slug:slug>/<str:key_id>', KSPKeyDetailView.as_view(), name='ksp_key'),
 ]
